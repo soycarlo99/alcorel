40bd509076e7cfeb1a63ed82e3036229285fd3f2
carlodelos90 <carlodelos90@gmail.com>
2025-02-12 11:07:35 +0100

    main jsx tempelate

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import HomePage from "./HomePage";
+
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
