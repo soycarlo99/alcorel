47ba4efa05559871a265cfeb390ac3e8d3988afa
patrikstudent <patrik.ryden@student.nbi-handelsakademin.se>
2025-02-13 13:47:12 +0100

    Wip, added view ticket function

--- src/EmployeeTicket.jsx
+++ src/EmployeeTicket.jsx
@@ -1,2 +1,28 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+export default function EmployeeTicket(){
+
+
+
+
+    useEffect(() =>  {
+  async function fetchTicket(){
+      const response = await fetch("/api/tickets");
+      const body = await response.json();
+      //{body} = json;
+      console.log(body);
+
+
+
+  }
+fetchTicket()
+ 
+    },[]);
+ 
+    return <>
+    
+    <h1>hejfghfghf</h1>
+     
+        
+</>;
+}
