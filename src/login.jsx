a4af03fd07ba0c1f8093c765b965ffd639dda2bc
Albin Martinsson <albinmartinsson@MacBook-Air-som-tillhor-Albin.local>
2025-02-13 09:50:51 +0100

    merge from main

---
+++ src/login.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
