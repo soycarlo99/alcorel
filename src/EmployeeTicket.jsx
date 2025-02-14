1fa0b3cfd37190712e927eb5a8e20e2641f57334
carlodelos90 <carlodelos90@gmail.com>
2025-02-14 11:33:14 +0100

    WIP

--- src/EmployeeTicket.jsx
+++ src/EmployeeTicket.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-export default function EmployeeTicket(){
 
+export default function EmployeeTicket() {
+  const [tickets, setTickets] = useState([]);
 
-
-
-    useEffect(() =>  {
-  async function fetchTicket(){
+  useEffect(() => {
+    async function fetchTicket() {
       const response = await fetch("/api/tickets");
       const body = await response.json();
-      //{body} = json;
       console.log(body);
+      setTickets(body);
+    }
+    fetchTicket();
+  }, []);
 
-
-
-  }
-fetchTicket()
- 
-    },[]);
- 
-    return <>
-    
-    <h1>hejfghfghf</h1>
-     
-        
-</>;
+  return (
+    <>
+      <h1>Employeee Tickets</h1>
+      {tickets.map((ticket, index) => (
+        <div>
+          <h2>Ticket ID: {ticket.ticket_id}</h2>
+          <h3>Ticket time: {ticket.ticket_time}</h3>
+          <h3>Messages: {ticket.message}</h3>
+          <h3>Reasoning: {ticket.category_id}</h3>
+          <h3>Customer: {ticket.user_id}</h3>
+          <h3>Status: {ticket.status}</h3>
+        </div>
+      ))}
+    </>
+  );
 }
