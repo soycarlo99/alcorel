dd67d360325304e4dfcad36fd30d8c60663a1af1
Albin Martinsson <albinmartinsson@MacBook-Air-som-tillhor-Albin.local>
2025-02-20 10:27:13 +0100

    fixed small buggs

--- src/AdminView.jsx
+++ src/AdminView.jsx
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 
 export default function AdminView() {
   const [categories, setCategories] = useState([]);
+
   useEffect(() => {
-    async function GetCategory() {
-      try {
-        const response = await fetch("/api/GetCategory");
-        const body = await response.json();
-        setCategories(body);
-        console.log(body);
-      } catch (error) {
-        console.error(error);
-      }
-    }
     GetCategory();
   }, []);
 
+  async function GetCategory() {
+    try {
+      const response = await fetch("/api/GetCategory");
+      const body = await response.json();
+      setCategories(body);
+      console.log(body);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   function handleAddSubmit(event) {
     event.preventDefault();
     let data = new FormData(event.target);
     data = Object.fromEntries(data);
     data = JSON.stringify(data);
-    fetch("/api/postCategory", {
+    fetch("/api/PostCategory", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: data,
     }).then((response) => {
       if (response.ok) {
-        console.log("Category Added Succesfully")
+        GetCategory();
+        console.log("Category Added Succesfully");
       }
     });
   }
@@ -39,7 +41,7 @@ export default function AdminView() {
       method: "DELETE",
     }).then((response) => {
       if (response.ok) {
-        fetchQuestions();
+        GetCategory();
       }
     });
   }
@@ -47,7 +49,6 @@ export default function AdminView() {
   return (
     <div>
       <form className="form" onSubmit={handleAddSubmit}>
-
         <label>Add Category: </label>
 
         <input name="category_name" type="text" required />
@@ -59,12 +60,14 @@ export default function AdminView() {
       {categories.map((item, index) => (
         <div>
           <h3 key={index}>{item.category_name}</h3>
-          <form onSubmit={handleRemove} action={`/api/DeleteCategory/${item.id}`}>
+          <form
+            onSubmit={handleRemove}
+            action={`/api/DeleteCategory/${item.id}`}
+          >
             <input type="submit" value={`Remove Category ${item.id}`} />
           </form>
         </div>
       ))}
-
     </div>
   );
 }
