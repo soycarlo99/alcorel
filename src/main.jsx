f1bf960782ea2785bc17fbb836a5a3a113c3939e
-huggo <hugomanns1337@gmail.com>
2025-02-24 09:56:37 +0100

    REMOVE DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import EditCategories from "./EditCategories";
 import AddQuestions from "./AddQuestions";
